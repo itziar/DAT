792535d15cb6538e7aec32e42914204951c9c876
itziar <itziar276@gmail.com>
2014-04-22 13:14:19 +0200

    formulario completo. mirar como se saca los datos de g+

--- js/redes.js
+++ js/redes.js
@@ -1,6 +1,26 @@
-$(function() {
-$( "#gplus" ).click(function() {
-	console.log("here")
-$( "#form" ).slideUp();
+
+$(document).ready(function(){
+	$(function() {
+	var mostrarform=true;
+	$("#gplus").click(function() {
+		if (mostrarform){
+			console.log("show")
+			$("div" ).show("slow");
+			mostrarform=false;
+		}else{
+			console.log("hide")
+			$("div").hide("slow");
+			mostrarform=true;
+		}
+	});
+	});
+    $("#enviar").click( function() {     // Con esto establecemos la acción por defecto de nuestro botón de enviar.
+        if($("#nombre").val() == ""){
+        alert("El campo Apellidos no puede estar vacío.");
+        }else{
+        	nombre=$("#nombre").focus().val();
+        	alert(nombre+"lo que sale");
+    	}
+    });    
 });
-});
+
